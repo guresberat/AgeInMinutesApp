90e314f68ae8409f3a0ea3b1bdd9416e0e3ee68e
Beratcan Güreş <guresberatcan@gmail.com>
2021-05-22 17:38:59 +0300

    Selected Date completed

--- app/src/main/java/com/example/ageinminutesapp/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutesapp/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.example.ageinminutesapp
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectedDate = findViewById<Button>(R.id.selectedDate)
+        selectedDate.setOnClickListener { view -> clickDatePicker(view) }
+    }
+
+    fun clickDatePicker(view: View) {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(
+            view.context,
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                val tvSelectedDate = findViewById<TextView>(R.id.tvSelectedDate)
+                tvSelectedDate.text = selectedDate
+            },
+            year,
+            month,
+            day
+        ).show()
     }
 }
+
